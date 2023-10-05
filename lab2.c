6fafe2ba363edda4f44a91df05bc0269c495fd4f
Nisarg70968 <146726749+Nisarg70968@users.noreply.github.com>
2023-10-05 18:23:00 +0530

    lab2
    update file

---
+++ lab2.c
@@ -0,0 +1,384 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.1
+
+    printf("Name: Nisarg D. Patel\n");
+    printf("Age: 23\n");
+    printf("School: Red & White\n");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.2
+
+    printf("\t-\t-\t-\t-\t-\t\n");
+    printf("|\t\t\t\t\t\t|\n");
+    printf("|\t\t\t\t\t\t|\n");
+    printf("|\t\t\tNISARG\t\t\t|\n");
+    printf("|\t\t\t\t\t\t|\n");
+    printf("|\t\t\t\t\t\t|\n");
+    printf("\t-\t-\t-\t-\t-\t\n");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.3
+
+//     printf("|  |\nN  |\nI  |\nS  |\nA  |\nR  |\nG  |\n|  |\n");
+
+    printf("\t-\t-\t-\t\n");
+    printf("|\t\t\t\t|\n");
+    printf("N\t\t\t\t|\n");
+    printf("I\t\t\t\t|\n");
+    printf("S\t\t\t\t|\n");
+    printf("A\t\t\t\t|\n");
+    printf("R\t\t\t\t|\n");
+    printf("G\t\t\t\t|\n");
+    printf("|\t\t\t\t|\n");
+    printf("\t-\t-\t-\t\n");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.4
+
+    printf("*\n\v");
+    printf("*\t*\n\v");
+    printf("*\t*\t*\n\v");
+    printf("*\t*\n\v");
+    printf("*\n\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.5
+
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t*\n\v\v");
+    printf("*\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\t*\t\t\t\t\t\t\t*\n\v\v");
+    printf("*\t\t\t*\t\t\t*\t\t\t\t\t*\n\v\v");
+    printf("*\t\t*\t\t\t\t\t*\t\t\t*\n\v\v");
+    printf("*\t*\t\t\t\t\t\t\t*\t*\n\v\v");
+    printf("*\n");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.6
+
+    printf("*\t*\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t*\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.7
+
+    printf("*\t*\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t*\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.8
+
+    printf("*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.9
+
+    printf("*\t*\n\v\v");
+    printf("*\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t  *\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t*\n\v\v");
+    printf("*\t*\n\v\v");
+
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+    
+    // Answer-2.10
+
+    printf("*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.11
+
+    printf("*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.12
+
+    printf("\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("\t*\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.13
+
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t*\t*\t*\n\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.14
+
+    printf("*\t*\t*\t*\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("*\t*\t*\t*\t*\n\v\v");    
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.15
+
+    printf("\t*\t*\t*\t*\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.16
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t*\n\v\v");
+    printf("*\t*\n\v\v");
+    printf("*\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.17
+
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.18
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t*\t\t*\t*\n\v\v");
+    printf("*\t\t*\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.19
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t*\t\t\t*\n\v\v");
+    printf("*\t\t*\t\t*\n\v\v");
+    printf("*\t\t\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.20
+
+    printf("\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("\t*\t*\t*  \n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.21
+
+    printf("*\t*\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t*\t* \n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.22
+
+    printf("\t*\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t*\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("\t*\t*\t\t*\n\v\v");
+
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.23
+
+    printf("*\t*\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t*\t* \n\v\v");
+    printf("*\t\t*\n\v\v");
+    printf("*\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.24
+
+    printf("\t*\t*\t*\t*\n\v\v");
+    printf("*\n\v\v");
+    printf("*\n\v\v");
+    printf("\t*\t*\t*\n\v\v");
+    printf("\t\t\t\t*\n\v\v");
+    printf("\t\t\t\t*\n\v\v");
+    printf("*\t*\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.25
+
+    printf("*\t*\t*\t*\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.26
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("\t*\t*\t*  \n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.27
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("\t*\t\t*\t\n\v\v");
+    printf("\t\t*\t\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.28
+
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+    printf("*\t\t*\t\t*\n\v\v");
+    printf("*\t*\t\t*\t*\n\v\v");
+    printf("*\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.29
+
+    printf("*\t\t\t\t\t\t*\n\v\v");
+    printf("\t*\t\t\t\t*\n\v\v");
+    printf("\t\t*\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t*\t\t*\n\v\v");
+    printf("\t*\t\t\t\t*\n\v\v");
+    printf("*\t\t\t\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.30
+
+    printf("*\t\t\t\t\t\t*\n\v\v");
+    printf("\t*\t\t\t\t*\n\v\v");
+    printf("\t\t*\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+    // Answer-2.31
+
+    printf("*\t*\t*\t\t*\t\t*\n\v\v");
+    printf("\t\t\t\t\t*\n\v\v");
+    printf("\t\t\t\t*\n\v\v");
+    printf("\t\t\t*\n\v\v");
+    printf("\t\t*\n\v\v");
+    printf("\t*\n\v\v");
+    printf("*\t\t*\t\t*\t*\t*\n\v\v");
+
+    printf("-------------------------------------------------------------------------------------------------------------\n\v\v");
+
+     return 0;
+      }
